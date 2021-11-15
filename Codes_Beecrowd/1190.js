237e5b2dfdf87a2029f1fde8d07cff67e32e1208
laura-bluz <laura.bluz@hotmail.com>
2021-11-15 14:04:19 -0300

    Incluindo o 1190

---
+++ Codes_Beecrowd/1190.js
@@ -0,0 +1,26 @@
+var input = require('fs').readFileSync('/Users/User/Desktop/Judge/Beecrowd/Codes_Beecrowd/stdin', 'utf8');
+var lines = input.split('\n');
+
+let op = lines[0].split('\n').map(item => item);
+op = op[0].trim()
+
+var vet = [], k = 0, soma = 0, media = 0;
+let mat = [], aux = 11;
+
+vet = input.split('\n').map(item => parseFloat(item));
+vet.shift()
+
+for (let li = 0; li < 12; li++) {
+    mat[li] = [];
+    for (col = 0; col < 12; col++) {
+        mat[li][col] = vet[k];
+        if (col > li && col > aux)
+            soma = mat[li][col] + soma;
+        k++;
+    }
+    aux--;
+}
+if (op === 'S')
+    console.log(soma.toFixed(1));
+else if (op === 'M')
+    console.log((soma / 30).toFixed(1));
